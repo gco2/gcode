ab54245aa4023cb8d8a467a96aedb00788a7d8f0
gcode <gcode.wav@gmail.com>
2021-02-04 18:32:04 +0100

    added Log.jsx

---
+++ src/components/Log.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Log extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  componentDidMount () {
+    //let _this = this;
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  
+  render() {
+    return (        
+        <div className="log">
+            logs
+        </div>
+    );
+  }
+}
+
+export default Log;
