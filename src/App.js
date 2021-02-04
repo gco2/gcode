82bfbb7c85db712177f065d1ebf71157800f787a
gcode <gcode.wav@gmail.com>
2021-02-04 18:30:59 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TerminalWindow from './components/TerminalWindow'
+import Tree from './components/Tree'
+import Desktop from './components/Desktop'
+import Log from './components/Log'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  componentDidMount () {
+    //let _this = this;
+
+    this.treeRef = React.createRef();
+    this.terminalRef = React.createRef();
+    this.desktopRef = React.createRef();
+    this.logRef = React.createRef();
+  }
+
+  componentWillUnmount() {
+
+  }
+  
+  showMsg = () => 'Hello World'
+
+  render() {
+    return (
+      <div className="app">
+
+        <div className="left-panel">
+          <TerminalWindow ref={this.terminalRef} />
+          <Tree ref={this.treeRef} />
+        </div>
+        
+        <div className="center-panel">
+          <Desktop ref={this.desktopRef} />
+          <div className="footer">
+            footer
+          </div>
+        </div>
+
+        <div className="right-panel">
+          <Log reg={this.logRef} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
