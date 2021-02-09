438cb16626507273a9e34435902d1e2677156b78
gcode <gcode.wav@gmail.com>
2021-02-09 14:11:09 +0100

    updated Me component

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import Draggable from 'react-draggable';
+
+let Window;
 
 class Me extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-
-    };
+    
+    Window = props.window;
   }
 
   componentDidMount () {
@@ -19,32 +18,13 @@ class Me extends React.Component {
 
   }
 
-  eventLogger = (e: MouseEvent, data: Object) => {
-    console.log('Event: ', e);
-    console.log('Data: ', data);
-  };
-  
   render() {
     return (
-      <Draggable
-        axis="both"
-        bounds=".desktop"
-        // handle=".handle"
-        defaultClassName="window"
-        defaultClassNameDragging="window-dragging"
-        defaultClassNameDragged="window-dragged"
-        defaultPosition={{x: 0, y: 0}}
-        // position={null}
-        grid={[50, 50]}
-        scale={1}
-        onStart={this.handleStart}
-        onDrag={this.handleDrag}
-        onStop={this.handleStop}>
-        <div>
-          <div className="handle">Drag from here</div>
+      <Window>
+        <div id="readme">
           <div>This readme is really dragging on...</div>
         </div>
-      </Draggable>
+      </Window>
     );
   }
 }
