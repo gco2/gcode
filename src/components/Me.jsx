8b15987bd3aecb661d2c05193726846d5945d334
gcode <gcode.wav@gmail.com>
2021-02-15 18:32:37 +0100

    added readme element

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -20,11 +20,21 @@ class Me extends React.Component {
 
   render() {
     return (
-      <Window>
-        <div id="readme">
-          <div>This readme is really dragging on...</div>
-        </div>
-      </Window>
+      <div id="windows-me">
+        <Window>
+          <div id="readme">
+            <div>
+              <p>Hi, my name is Guillaume Piccarreta.</p>
+              <p>Welcome to my web portal interface.</p>
+              <p>I am a software developer, teacher, audio and visual artist working with code.</p>
+              <p>I like to make experimental software, write and fix bugs while listenning to white noise with a lowpass filter cutoff frequency set to 440Hz.</p>
+              <p>Feel free to contact me at <a href="mailto:gcode.wav@gmail.com">gcode.wav@gmail.com</a></p>
+              <p>Have a nice day :)</p>
+            </div>
+          </div>
+        </Window>
+
+      </div>
     );
   }
 }
