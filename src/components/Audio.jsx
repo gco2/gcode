9177266fdbb9aa93756317d5f43a7b5590a12ec7
gcode <gcode.wav@gmail.com>
2021-02-25 01:02:33 +0100

    added oscilloscope

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Oscilloscope from 'oscilloscope';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import track01 from '../assets/audio/5267TR98Y28_rev2(psx).mp3'
@@ -11,12 +12,14 @@ class Audio extends React.Component {
     this.player = React.createRef();
 
     this.state = {
-      track: ""
+      track: "",
+      audioContext: new window.AudioContext()
     };
 
     this.getTrackName = this.getTrackName.bind(this);
     this.addTrackNameMarquee = this.addTrackNameMarquee.bind(this);
     this.updateAudio = this.updateAudio.bind(this);
+    this.startOscilloscope = this.startOscilloscope.bind(this);
     this.onPlay = this.onPlay.bind(this);
   }
 
@@ -28,9 +31,11 @@ class Audio extends React.Component {
       name: ""
     })
 
-    this.addTrackNameMarquee("???? - ????");
+    this.addTrackNameMarquee("♫ ♪ ♫ ♪ ");
 
     this.updateAudio();
+
+    this.startOscilloscope(this.state.audioContext);
   }
 
   componentWillUnmount() {
@@ -72,8 +77,53 @@ class Audio extends React.Component {
     });
   }
 
+  startOscilloscope = (context) => {
+    //const audioContext = new window.AudioContext()
+    
+
+    // setup canvas
+    const canvas = document.createElement('canvas')
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight - 50
+    document.body.appendChild(canvas)
+  
+    // // setup audio element
+    // const audioElement = document.createElement('audio')
+    // audioElement.controls = true
+    // audioElement.autoplay = true
+    // audioElement.src = track01
+    // document.body.appendChild(audioElement)
+  
+    // create source from html5 audio element
+    const source = context.createMediaElementSource(this.player.current.audio.current)
+    console.log(source)
+  
+    // attach oscilloscope
+    const scope = new Oscilloscope(source)
+    
+    // reconnect audio output to speakers
+    source.connect(context.destination)
+    console.log(context)
+
+    // customize drawing options
+    const ctx = canvas.getContext('2d')
+    ctx.lineWidth = 2
+    ctx.strokeStyle = '#ffffff'
+  
+    // start default animation loop
+    scope.animate(ctx)
+  
+  }
+
   onPlay = (e) => {
     console.log(e)
+
+    if(this.state.audioContext.state === 'suspended') {
+      this.state.audioContext.resume().then(function() {
+
+      });
+    }
+  
   }
 
   render() {
