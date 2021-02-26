382d698f60bc56ab41fd228165a93fdd266a8429
gcode <gcode.wav@gmail.com>
2021-02-26 13:37:06 +0100

    added clock

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Clock from 'react-digital-clock';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  componentDidMount () {
+    //let _this = this;
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  
+  render() {
+    return (        
+      <div className="header">
+        <div>gcode</div>
+        <div className="clock"><Clock /></div>
+      </div>
+    );
+  }
+}
+
+export default Header;
