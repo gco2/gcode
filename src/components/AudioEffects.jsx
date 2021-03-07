c64d4392acd374eff295357e290d5954eb67d293
gcode <gcode.wav@gmail.com>
2021-03-07 19:50:38 +0100

    audio effects update

--- src/components/AudioEffects.jsx
+++ src/components/AudioEffects.jsx
@@ -7,12 +7,16 @@ class AudioEffects extends React.Component {
     super(props);
 
     this.state = {
-      
+      pitch: 0.0,
+      distortion: 0.0,
+      delay: 0.0,
+      reverb: 0.0
     };
 
     this.updatePitch = this.updatePitch.bind(this);
     this.updateDistortion = this.updateDistortion.bind(this);
     this.updateDelay = this.updateDelay.bind(this);
+    this.updateReverb = this.updateReverb.bind(this);
   }
 
   componentDidMount (props) {
@@ -24,24 +28,66 @@ class AudioEffects extends React.Component {
   }
 
   updatePitch = (e) => {    
-    this.props.audio.current.state.pitch.pitch = parseFloat(e.target.value);
+    let val = parseFloat(e.target.value);
+    this.props.audio.current.state.pitch.pitch = val;
     // console.log(this.props.audio.current.state.pitch.pitch)
+
+    this.setState({
+      pitch: val
+    })
   }
 
   updateDistortion = (e) => {
-    this.props.audio.current.state.distortion.distortion = parseFloat(e.target.value)
+    let val = parseFloat(e.target.value);
+    this.props.audio.current.state.distortion.distortion = val;
+
+    this.setState({
+      distortion: val
+    })
   }
 
   updateDelay = (e) => {
-    this.props.audio.current.state.delay.delayTime.value = parseFloat(e.target.value)
+    let val = parseFloat(e.target.value);
+    this.props.audio.current.state.delay.delayTime.value = val;
+
+    this.setState({
+      delay: val
+    })
+  }
+
+  updateReverb = (e) => {
+    let val = parseFloat(e.target.value);
+    this.props.audio.current.state.reverb.decay = val;
+
+    this.setState({
+      reverb: val
+    })
   }
 
   render() {
     return (
       <div id="audio-effects">
-        <input type="range" id="pitch" label="Pitch" min="0.0" max="24.0" defaultValue="0.0" value={this.state.value} step="0.5"  units="hz" onChange={this.updatePitch} />
-        <input type="range" id="distortion" label="Distortion" min="0.0" max="1.0" defaultValue="0.0" value={this.state.value} step="0.01"  units="dB" onChange={this.updateDistortion} />
-        <input type="range" id="delay" label="Delay" min="0.0" max="1.0" defaultValue="0.0" value={this.state.value} step="0.01"  units="ms" onChange={this.updateDelay} />
+        <div className="audio-effects-title">dsp</div>
+        <div className="audio-effects-row">
+          <div className="audio-fader">
+            <input className="audio-effects-input" type="range" id="pitch" label="Pitch" min="0.0" max="24.0" defaultValue="0.0" value={this.state.value} step="0.01"  units="hz" onChange={this.updatePitch} />
+            <label className="audio-effects-label">pitch {this.state.pitch}hz</label>
+          </div>
+          <div className="audio-fader">
+            <input className="audio-effects-input" type="range" id="distortion" label="Distortion" min="0.0" max="1.0" defaultValue="0.0" value={this.state.value} step="0.01"  units="dB" onChange={this.updateDistortion} />
+            <label className="audio-effects-label">noise {this.state.distortion}dB</label>
+          </div>
+        </div>
+        <div className="audio-effects-row">
+          <div className="audio-fader">
+            <input className="audio-effects-input" type="range" id="delay" label="Delay" min="0.0" max="1.0" defaultValue="0.0" value={this.state.value} step="0.01"  units="ms" onChange={this.updateDelay} />
+            <label className="audio-effects-label">delay {this.state.delay}ms</label>
+          </div>
+          <div className="audio-fader">
+            <input className="audio-effects-input" type="range" id="reverb" label="reverb" min="0.001" max="10.0" defaultValue="0.01" value={this.state.value} step="0.1"  units="%" onChange={this.updateReverb} />
+            <label className="audio-effects-label">reverb: {this.state.reverb}%</label>
+          </div>
+        </div>
       </div>
     );
   }
